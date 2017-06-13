76fcc80fd9c024f85d9eaf60b9257d2679f53b7f
Dyas <dyas@live.com>
2017-06-13 14:35:40 +0700

    add permission

--- app/Http/Controllers/ActivitiesController.php
+++ app/Http/Controllers/ActivitiesController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class ActivitiesController extends Controller
 {
     public function showAll($username) {
     	$user = \App\User::whereName($username)->firstOrFail();
-    	$activities = $user->activity()->with(['user', 'subject'])->get();
+    	if (Auth::user()->name == $user->name) {
+	    	$activities = $user->activity()->with(['user', 'subject'])->get();
 
-    	return view('activity', compact('activities'));
+	    	return view('activity', compact('activities'));
+	    } else return "You are not logged in as " . $username;
     }
 }
