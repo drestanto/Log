95f6b8477c9e81e0114a2ea7f3718929cf5122f3
Dyas <dyas@live.com>
2017-06-13 12:47:33 +0700

    getActName

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -13,12 +13,16 @@ trait RecordsActivity
     		Activity::create([
     			'subject_id' => $model->id,
     			'subject_type' => get_class($model),
-    			'name' => $model->getActivityName(),
+    			'name' => $model->getActivityName($model, "created"),
     			'user_id' => $model->user_id
     		]);
 
     	});
 	}
 
-	
+	protected function getActivityName($model, $action)
+	{
+		$name = strtolower((new ReflectionClass)->getShortName());
+		return $action . "_" . $name;
+	}
 }
