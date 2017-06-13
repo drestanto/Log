fb8410e55458a40dcddbbbcabea9f18d2f206bed
Dyas <dyas@live.com>
2017-06-13 14:32:45 +0700

    repair view

--- resources/views/activity.blade.php
+++ resources/views/activity.blade.php
@@ -3,10 +3,10 @@
 	<h2> Your Activities </h2>
 	@foreach ($activities as $activity)
 		@if ($activity->name == 'created_post')
-			{{ $activity->user->name }} published a post on {{ $event->created_at->diffForHumans() }}
+			{{ $activity->user->name }} published a post {{ $activity->created_at->diffForHumans() }}<br>
 		@endif
 		@if ($activity->name == 'created_comment')
-			{{ $activity->user->name }} left a comment on {{ $event->created_at->diffForHumans() }}
+			{{ $activity->user->name }} left a comment {{ $activity->created_at->diffForHumans() }}<br>
 		@endif
 	@endforeach
 </body>
