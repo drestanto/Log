32c88a52eba1f202f9527833f6eaa10139c60db1
Dyas <dyas@live.com>
2017-06-13 12:42:20 +0700

    migrate function boot()

--- app/Post.php
+++ app/Post.php
@@ -8,21 +8,6 @@ class Post extends Model
 {
     use RecordsActivity;
 
-    protected static function boot()
-    {
-    	parent::boot();
-
-    	static::created(function($post) {
-    		Activity::create([
-    			'subject_id' => $post->id,
-    			'subject_type' => get_class($post),
-    			'name' => 'created_post',
-    			'user_id' => $post->user_id
-    		]);
-
-    	});
-    }
-
     /**
      * The attributes that are mass assignable.
      *
--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -7,6 +7,16 @@ trait RecordsActivity
 
 	protected static function boot()
 	{
-		
+    	parent::boot();
+    	
+    	static::created(function($post) {
+    		Activity::create([
+    			'subject_id' => $post->id,
+    			'subject_type' => get_class($post),
+    			'name' => 'created_post',
+    			'user_id' => $post->user_id
+    		]);
+
+    	});
 	}
 }
