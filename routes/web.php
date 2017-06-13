cff2d2959c8d5d9ca1a2f23d5063ea62a4d32cf8
Dyas <dyas@live.com>
2017-06-13 13:54:15 +0700

    Activities Show

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('users/{username}/activity', 'ActivitiesController@showAll');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
